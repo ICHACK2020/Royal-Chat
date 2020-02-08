7679267980cdf519590f02e474ade7f2220573ba
Akshat Tripathi <at619@ic.ac.uk>
2020-02-08 19:28:36 +0000

    added client.py as temp

--- pages.go
+++ pages.go
@@ -1,7 +1,14 @@
 package main
 
-import "http"
+import (
+	"html/template"
+	"http"
+)
 
 func homepageHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("./html/index.html")
+	if err != nil {
+		panic(err)
+	}
 
 }

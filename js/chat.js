b11bd863d441ff9c779481e9e5830016403a1988
HenryHu2000 <henryhu2000@outlook.com>
2020-02-08 13:06:32 +0000

    Update WebSocket address in JavaScript

--- js/chat.js
+++ js/chat.js
@@ -3,5 +3,22 @@
  */
 
 (function() {
+  var socket = new WebSocket("ws://146.169.207.172:8080");
+  
+  socket.onopen = function(e) {
+    console.log("Success!");
+    socket.send("Hello!");
+  }
+  socket.onmessage = function(e) {
+    console.log(e.data);
 
+  }
+  socket.onclose = function(e) {
+    
+  }
+  socket.onerror = function(e) {
+    
+  }
+
+  
 })();

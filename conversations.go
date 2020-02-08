2ce951b1b74029fdcc7576b3f54265899a2414bc
Akshat Tripathi <at619@ic.ac.uk>
2020-02-08 14:10:33 +0000

    added conversation init

--- conversations.go
+++ conversations.go
@@ -1,8 +1,23 @@
 package main
 
-import "github.com/gorilla/websocket"
+import (
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
 
 type conversation struct {
-	Conn *websocket.Conn
-	ID   int
+	conn     *websocket.Conn
+	messages []message
+}
+
+func newConversation(w http.ResponseWriter, r *http.Request) *conversation {
+	socket, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		panic(err)
+	}
+	defer socket.Close()
+	msgs := make([]message, 30) //30 is the default length of messages
+	return &conversation{conn: socket,
+		messages: msgs}
 }

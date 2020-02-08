f842c5b982db12bec50e71fea43660e75ee7e830
HenryHu2000 <henryhu2000@outlook.com>
2020-02-08 20:15:51 +0000

    Finish chat page layout

--- js/chat.js
+++ js/chat.js
@@ -1,8 +1,33 @@
-/**
- * 
- */
+"use strict";
+ 
+var socket;
+var id;
 
-(function() {
+function sendMessage() {
+  let chatArea = document.getElementById("chatArea");
+  socket.send(id + ": " + chatArea.value);
+  chatArea.value = "";
+}
+
+function addMyMessage(message) {
+  let messageBox = document.getElementById("messageBox");
+  messageBox.insertAdjacentHTML("beforeend", '<div class="panel message myMessage"><div class="panel-body"></div></div>');
+  printMessage(message);
+}
+
+function addOtherMessage(message) {
+  let messageBox = document.getElementById("messageBox");
+  messageBox.insertAdjacentHTML("beforeend", '<div class="panel message otherMessage"><div class="panel-body"></div></div>');
+  printMessage(message);
+}
+
+function printMessage(message) {
+  var node = document.createTextNode(message);
+  document.getElementById("messageBox").lastChild.firstChild.appendChild(node);
+  window.scrollTo(0, document.body.scrollHeight);
+}
+
+function initializeSocket() {
   var socket = new WebSocket("ws://146.169.207.172:8080");
   
   socket.onopen = function(e) {
@@ -20,6 +45,23 @@
   socket.onerror = function(e) {
     
   }
+  return socket;
+}
 
-  
-})();
+document.onkeydown = function (event) {
+  if (event.key === "Enter") {
+    sendMessage();
+    return false;
+  }
+};
+
+function handleResize() {
+  $(".myMessage").css("left", (window.innerWidth >= 390 ? window.innerWidth - 390 : 0) + "px");
+}
+
+window.onresize = function (event) {
+  handleResize();
+};
+
+socket = initializeSocket();
+handleResize();

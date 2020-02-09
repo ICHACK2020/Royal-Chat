d1a8f28c336801fb2d1ac40ff7c67a1f6403dc5b
George Stacey <gs919@ic.ac.uk>
2020-02-09 04:53:00 +0000

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	js/chat.js
    slight change to troll detection so the score returned is harsher on non-profanity things

--- troll_detection/for_my_amusement.py
+++ troll_detection/for_my_amusement.py
@@ -1,17 +1,7 @@
 from tracker import Detector, Score
 
-msgs = ["I have never seen anything so despicable before",
-        "If you walked into a bar you would hit your head",
-        "why are you offended you're such a snowflake",
-        "When you speak I want to blow your brains out",
-        "wow. genius. fucking brilliant",
-        "I hope your son gets cancer",
-        "I'm going to trace your location and find you",
-        "If you don't believe in God, he will punish you in hell forever",
-        "We need to take the country forward. make britain great again. We don't need the EU.",
-        "That's so gay",
-        "Well no obviously not, I think it's a little more subtle than that",
-        "fuck you"]
+msgs = ["Fuck you fag",
+        "I will kill you in your sleep"]
 
 for msg in msgs:
     d = Detector()
